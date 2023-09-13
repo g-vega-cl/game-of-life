0d797f0e2bb9eb92ea2900efe66c095c11398c95
Cesar Vega <g.vega.cl@gmail.com>
2023-09-13 11:06:26 -0400

    move styles up

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,22 +3,21 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { ChakraProviders, SessionProviders } from "./providers";
 
-
-const RootLayout = async ({
-  children,
-}: {
-  children: React.ReactNode;
-}) =>  {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await getServerSession(authOptions);
   return (
     <html lang="en">
       <body className="bg-white">
         <SessionProviders session={session}>
-          <ChakraProviders>{children}</ChakraProviders>
+          <ChakraProviders>
+            <div className="bg-white text-black h-full w-full min-h-screen	min-w-screen overflow-x-scroll">
+              {children}
+            </div>
+          </ChakraProviders>
         </SessionProviders>
       </body>
     </html>
   );
-}
+};
 
-export default RootLayout
+export default RootLayout;
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,11 +4,11 @@ import { GameOfLife } from "./components/organisms/gameOfLife/GameOfLife";
 import { Box } from "@chakra-ui/react";
 export default function Home() {
   return (
-    <main className="h-full w-full min-h-screen	min-w-screen bg-white overflow-x-scroll">
+    <Box >
       <Navbar />
       <Box className="p-8">
         <GameOfLife />
       </Box>
-    </main>
+    </Box>
   );
 }
